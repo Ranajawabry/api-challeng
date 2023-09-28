9913b139675e83d0383227f199ec7f6174e9f921
Ranajawabry <121602339+Ranajawabry@users.noreply.github.com>
2023-09-28 15:53:18 +0300

    Update main.js

--- assests/js/main.js
+++ assests/js/main.js
@@ -26,61 +26,61 @@ const display= (users)=>{
        
         result+=`
 
-        <div class="single-item col-lg-4 col-md-6  bg-secondary rounded-3 my-1">
-                <div class="row p-2" >
-                    <div class="col-3">
-                        <img src=${user.avatar_url} class="w-100" alt="">
+        <div class="single-item col-xl-6   rounded-3 my-1">
+        <div class="row p-2" >
+            <div class="col-3">
+                <img src=${user.avatar_url} class="w-100 rounded-circle" alt="">
+            </div>
+            <div class="col-9 ps-4">
+                <div class="titel d-flex justify-content-between gap-3">
+                    <div>
+                        <h2 class="text-white">${user.login}</h2>
                     </div>
-                    <div class="col-9 ps-4">
-                        <div class="titel d-flex justify-content-between gap-3">
-                            <div>
-                                <h2 class="text-white">${user.login}</h2>
-                            </div>
-                            <div>
-                                <p class="text-white"> 22/5/45433</p>
-                            </div>
-                        </div>
-                        <div class="baio my-1">
-                            <p class="fs-4">${userInfo.bio}</p>
-                        </div>
-                        <div class="follow d-flex justify-content-between bg-dark text-white p-3 rounded-2">
-                            <div class="follower">
-                                <p class="fs-5">Follower</p>
-                                <p>${userInfo.followers}</p>
-                            </div>
-                            <div class="following">
-                                <p class="fs-5">following</p>
-                                <p>${userInfo.following}</p>
-                            </div>
-                            <div class="repo">
-                                <p class="fs-5">repos</p>
-                                <p>${userInfo.public_repos}</p>
-                            </div>
-                        </div>
-                        <div class="d-flex flex-wrap spcial-info my-3">
-                            <div class="col-6 my-2">
-                                <i class="fa-solid fa-location-dot pe-5 fs-5"></i>
-                                <span> ${user.location} </span>
-                            </div>
-                            <div class="col-6 my-2">
-                                <i class="fa-brands fa-twitter pe-5 fs-5"></i>
-                                <span>${user.twitter_username}</span>
-                            </div>
-                            <div class="col-6 my-2">
-                                <a href=${user.blog}><i class="fa-solid fa-house-user pe-5 fs-5"></i></a>
-                                <span> Blogs </span>
-                            </div>
-                            <div class="col-6 my-2">
-                                <a href=${user.html_url}><i class="fa-solid fa-house-user pe-5 fs-5"></i></a>
-                                <span> github</span>
-                            </div>
-
-                        </div>
-                      
+                    <div>
+                        <p class="text-white"> 22/5/45433</p>
                     </div>
                 </div>
-            
+                <div class="baio my-1 text-white">
+                    <p class="fs-4">${userInfo.bio}</p>
+                </div>
+                <div class="follow d-flex justify-content-between bg-dark text-white p-3 ">
+                    <div class="follower">
+                        <p class="fs-5">Follower</p>
+                        <p>${userInfo.followers}</p>
+                    </div>
+                    <div class="following">
+                        <p class="fs-5">following</p>
+                        <p>${userInfo.following}</p>
+                    </div>
+                    <div class="repo">
+                        <p class="fs-5">repos</p>
+                        <p>${userInfo.public_repos}</p>
+                    </div>
+                </div>
+                <div class="row spcial-info my-3 text-white">
+                    <div class="col-xl-6 my-2">
+                        <i class="fa-solid fa-location-dot pe-5 fs-5 text-white"></i>
+                        <span> ${user.location} </span>
+                    </div>
+                    <div class="col-xl-6 my-2">
+                        <i class="fa-brands fa-twitter pe-5 fs-5 text-white"></i>
+                        <span>${user.twitter_username}</span>
+                    </div>
+                    <div class="col-xl-6  my-2">
+                        <a href=${user.blog}><i class="fa-solid fa-house-user pe-5 fs-5 text-white"></i></a>
+                        <span> Blogs </span>
+                    </div>
+                    <div class="col-xl-6 my-2">
+                        <a href=${user.html_url}><i class="fa-solid fa-house-user pe-5 fs-5 text-white"></i></a>
+                        <span> github</span>
+                    </div>
+
+                </div>
+              
             </div>
+        </div>
+    
+    </div>
         `
     })
 
