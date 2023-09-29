a7a97ce59bef70e27df3efc30bc16af74a7429df
Ranajawabry <rana.a.jawabry@gmail.com>
2023-09-29 20:48:50 +0300

    update time

--- assests/js/main.js
+++ assests/js/main.js
@@ -10,6 +10,7 @@ const debounce = (func, timeout = 500) => {
 
 const usersUrl = "https://api.github.com/users";
 const search = document.getElementById("search");
+const mood = document.getElementById('mood');
 console.log(search)
 
 const searchInput = () => {
@@ -33,6 +34,10 @@ async function getData(seachValue) {
 
 
 }
+const formatDate = (date) => {
+    const options = { year: 'numeric', month: 'short', day: 'numeric' };
+    return new Date(date).toLocaleDateString('en-US', options);
+}
 
 const display = (user) => {
    console.log(user)
@@ -49,23 +54,23 @@ const display = (user) => {
                         <h2 class="text-white">${user.login}</h2>
                     </div>
                     <div>
-                        <p class="text-white">${user.created_at}</p>
+                        <p class="text-white pe-5 fs-4">${formatDate(user.created_at)}</p>
                        
                     </div>
                 </div>
                 <div class="baio my-1 text-white">
-                    <p class="fs-4">${user.bio}</p>
+                    <p class="fs-4">${user.bio ?? 'no bio exist...'}</p>
                 </div>
-                <div class="follow d-flex justify-content-around w-75 text-white py-4">
-                    <div class="follower">
+                <div class="follow d-flex justify-content-around  text-white py-4">
+                    <div class="follower text-capitalize">
                         <p class="fs-5">Follower</p>
                         <p>${user.followers}</p>
                     </div>
-                    <div class="following">
+                    <div class="following text-capitalize">
                         <p class="fs-5">following</p>
                         <p>${user.following}</p>
                     </div>
-                    <div class="repo">
+                    <div class="repo text-capetalize">
                         <p class="fs-5">repos</p>
                         <p>${user.public_repos}</p>
                     </div>
@@ -73,18 +78,18 @@ const display = (user) => {
                 <div class="row spcial-info my-3 text-white">
                     <div class="col-xl-6 my-2">
                         <i class="fa-solid fa-location-dot pe-2 fs-5 text-white"></i>
-                        <span> ${user.location} </span>
+                        <span> ${user.location ?? 'not available'} </span>
                     </div>
                     <div class="col-xl-6 my-2">
                         <i class="fa-brands fa-twitter pe-2 fs-5 text-white"></i>
-                        <span>${user.twitter_username}</span>
+                        <span>${user.twitter_username ?? 'Not Available'}</span>
                     </div>
                     <div class="col-xl-6  my-2">
                         <a href=${user.blog}><i class="fa-solid fa-link  pe-2 fs-5 text-white"></i></a>
                         <span> Blogs </span>
                     </div>
                     <div class="col-xl-6 my-2">
-                        <a href=${user.html_url}><i class="fa-solid fa-house-user pe-2 fs-5 text-white"></i></a>
+                        <a href=${user.html_url}><i class="fa-solid fa-building pe-2 fs-5 text-white"></i></a>
                         <span> github</span>
                     </div>
 
@@ -101,54 +106,15 @@ const display = (user) => {
 
 /////////////mood of site/////////
 
-const mood = document.getElementById('mood');
-mood.innerHTML=`
-<span class="text-uppercase pe-3" >light</span>
-<i class="fa-regular fa-sun"></i>`
-console.log(mood);
-let moodType
+
+let moodType;
  if(localStorage.getItem('mood')){
     moodType =localStorage.getItem("mood");
  }else{
     moodType ='dark'
  } 
-
-const changeMood = ()=>{
-
-    if(moodType=='dark'){
-    moodType = 'light';  // dark mood
-    localStorage.setItem("mood","light")
-    console.log(moodType);
-    console.log('dark');
-    mood.innerHTML=`<span class="text-uppercase pe-3" >dark</span>
-    <i class="fa-solid fa-moon"></i>`;
-    document.body.style.backgroundColor='rgb(217, 227, 235)';
-
-    mood.classList.remove('dark-mood')
-    mood.classList.add('light-mood')
-    document.querySelector('.single-item')?.classList.remove('single-item-dark');
-    document.querySelector('.single-item')?.classList.add('single-item-light');
-     document.querySelector('.follow')?.classList.remove('follow-dark');
-    document.querySelector('.follow')?.classList.add('follow-light');
-    document.querySelector('h1').classList.remove('text-white')
-    document.querySelector('h1').classList.add('text-dark')
-    document.querySelector('.spcial-info')?.classList.remove('text-white');
-    document.querySelector('.spcial-info')?.classList.add('text-dark');
-    document.querySelector('.baio')?.classList.remove('text-white');
-    document.querySelector('.baio')?.classList.add('text-dark');
-    document.querySelector('h2')?.classList.replace('text-white','text-dark')
-    document.querySelector('.navbar').style.backgroundColor= 'white';
-    document.querySelectorAll('i').forEach((item)=>{
-        item.classList.replace('text-white','text-dark');
-    })
-    document.querySelector('input').classList.replace('text-white','text-dark')
-
-   
-}
-   else{
-    console.log('light');
-    moodType='dark';
-    localStorage.setItem("mood","dark")
+ 
+ const darkMood = ()=>{
     mood.innerHTML=
     `<span class="text-uppercase pe-3" >light</span>
     <i class="fa-solid fa-sun"></i>`;
@@ -175,11 +141,63 @@ const changeMood = ()=>{
 
     
 
+}
+const lightMood= ()=>{
+    mood.innerHTML=`<span class="text-uppercase pe-3" >dark</span>
+    <i class="fa-solid fa-moon"></i>`;
+    document.body.style.backgroundColor='rgb(217, 227, 235)';
+
+    mood.classList.remove('dark-mood')
+    mood.classList.add('light-mood')
+    document.querySelector('.single-item')?.classList.remove('single-item-dark');
+    document.querySelector('.single-item')?.classList.add('single-item-light');
+     document.querySelector('.follow')?.classList.remove('follow-dark');
+    document.querySelector('.follow')?.classList.add('follow-light');
+    document.querySelector('h1').classList.remove('text-white')
+    document.querySelector('h1').classList.add('text-dark')
+    document.querySelector('.spcial-info')?.classList.remove('text-white');
+    document.querySelector('.spcial-info')?.classList.add('text-dark');
+    document.querySelector('.baio')?.classList.remove('text-white');
+    document.querySelector('.baio')?.classList.add('text-dark');
+    document.querySelector('h2')?.classList.replace('text-white','text-dark')
+    document.querySelector('.navbar').style.backgroundColor= 'white';
+    document.querySelectorAll('i').forEach((item)=>{
+        item.classList.replace('text-white','text-dark');
+    })
+    document.querySelector('input').classList.replace('text-white','text-dark')
+
+}
+mood.innerHTML=`
+<span class="text-uppercase pe-3" >${moodType}</span>
+<i class="fa-regular fa-sun"></i>`
+
+if(moodType=='dark'){
+    darkMood();
+}else{
+    lightMood();
+}
+console.log(mood);
+
+
+const changeMood = ()=>{
+
+    if(moodType=='dark'){
+    moodType = 'light';  // dark mood
+    localStorage.setItem("mood","light")
+    console.log(moodType);
+    console.log('dark');
+    lightMood();
+
    
+}
+   else{
+    console.log('light');
+    moodType='dark';
+    localStorage.setItem("mood","dark")
+    darkMood();
 
 }
 }
 mood.addEventListener('click',changeMood);
 const single = document.querySelector('.single-item');
 
-console.log(document.querySelector('input'))
